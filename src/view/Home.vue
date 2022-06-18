ecc33ffc0e9b783ac158b939b9223db6a4c96dbe
Seeya0 <vinlandtorfin@gmail.com>
2022-06-18 22:46:53 +0900

    マップを追加(Leaflet.jsの使用)

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col h-screen max-h-screen">
 
       <!-- 検索 結果表示部分 -->
-      <div class="flex justify-center relative bg-teal-300 bg-cover px-4 pt-8 pb-32">
+      <div class="z-20 flex justify-center relative bg-teal-300 bg-cover px-4 pt-8 pb-32">
 
         <!-- 検索バー -->
         <div class="w-full max-w-screen-sm">
@@ -21,22 +21,37 @@
           <IPInfo />
         </div>
       </div>
+
+      <!-- マップ -->
+      <div
+        id="mapid"
+        class="h-full z-10"
+      ></div>
     </div>
   </header>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, onMounted } from 'vue'
 import IPInfo from '../components/IPInfo.vue'
+import leaflet from 'leaflet';
 
 export default defineComponent({
   name: "Home",
   components: { IPInfo },
   setup() {
-    const weight = ref('');
-    const height = ref('')
-    return { weight, height };
-  },
+    let myMap;
+
+    onMounted(() => {
+      myMap = leaflet.map('mapid').setView([51.505, -0.09], 13)
+
+      leaflet.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${import.meta.env.VITE_MAPBOX_TOKEN}`, {
+        maxZoom: 19,
+        id: 'mapbox/streets-v11',
+        attribution: '© OpenStreetMap'
+      }).addTo(myMap);
+    })
+  }
 })
 </script>
 
