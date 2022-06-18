440a0ee79a49743f64b22550ff72f930509bcb6f
Seeya0 <vinlandtorfin@gmail.com>
2022-06-18 17:41:20 +0900

    IPアドレスの情報表示部分の追加

---
+++ src/components/IPInfo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="absolute -bottom-64 lg:-bottom-20 flex flex-col lg:flex-row bg-white gap-x-12 gap-y-4 px-8 pt-8 pb-12 mx-8 rounded-md"
+  >
+
+    <section>
+      <div class="flex flex-col">
+        <h3 class="text-xs mb-2 uppercase">IP Address</h3>
+        <span class="text-2xl">8.8.8.8</span>
+      </div>
+    </section>
+
+    <section>
+      <div class="flex flex-col">
+        <h3 class="text-xs mb-2 uppercase">Location</h3>
+        <span class="text-2xl">Tokyo</span>
+      </div>
+    </section>
+
+    <section>
+      <div class="flex flex-col">
+        <h3 class="text-xs mb-2 uppercase">Timezone</h3>
+        <span class="text-2xl">UTC -07:00</span>
+      </div>
+    </section>
+
+    <section>
+      <div class="flex flex-col">
+        <h3 class="text-xs mb-2 uppercase">ISP</h3>
+        <span class="text-2xl">Google</span>
+      </div>
+    </section>
+
+  </div>
+</template>
+
+<script>
+</script>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -4,7 +4,22 @@
 
       <!-- 検索 結果表示部分 -->
       <div class="flex justify-center relative bg-teal-300 bg-cover px-4 pt-8 pb-32">
-        aaa
+
+        <!-- 検索バー -->
+        <div class="w-full max-w-screen-sm">
+          <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+          <div class="flex">
+            <input
+              class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md bg-white focus:outline-none"
+              type="text"
+              placeholder="Search for any IP address or leave empty to get your ip info"
+            />
+            <i
+              class="fas fa-chevron-right cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center"></i>
+          </div>
+          <!-- IPの情報を表示 -->
+          <IPInfo />
+        </div>
       </div>
     </div>
   </header>
@@ -12,9 +27,11 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+import IPInfo from '../components/IPInfo.vue'
 
 export default defineComponent({
   name: "Home",
+  components: { IPInfo },
   setup() {
     const weight = ref('');
     const height = ref('')
